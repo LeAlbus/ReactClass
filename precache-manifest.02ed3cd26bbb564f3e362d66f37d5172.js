a132eeb76b3de995fb89d0da3ff291f1a6837809
Pedro Lopes <pedrolopes.dev@gmail.com>
2019-05-20 22:24:42 -0300

    Add files via upload

---
+++ precache-manifest.02ed3cd26bbb564f3e362d66f37d5172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d4db7140af5565b149a20d73789686",
+    "url": "/index.html"
+  },
+  {
+    "revision": "975fd3cf8ade25ce8993",
+    "url": "/static/css/main.6009b91b.chunk.css"
+  },
+  {
+    "revision": "cb44525308cbae5b3614",
+    "url": "/static/js/2.88617338.chunk.js"
+  },
+  {
+    "revision": "975fd3cf8ade25ce8993",
+    "url": "/static/js/main.a13de56f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
